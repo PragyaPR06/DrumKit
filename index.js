adcb24e49412e1e661a5f3ccc0dcdb439c0c62f6
PragyaPR06 <129600566+PragyaPR06@users.noreply.github.com>
2023-08-15 15:13:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-alert("Linked Succesfully!");
+
 
 for(var i=0; i<document.querySelectorAll("button").length; i++){
 document.querySelectorAll("button")[i].addEventListener("click",handleclick);
 }
 
 function handleclick(){
-    // alert("I got clicked!");
-    var audio = new  Audio('./sounds/tom-1.mp3');
+    var audio = new  Audio('./tom-1.mp3');
     audio.play();
 }
 
